3646e16749208c09899c2653289e8c3ee9b72fe6
Yosef Awad <yosef.awad.1@gmail.com>
2022-05-27 21:03:50 +0300

    added checking user and signing in

--- script/singin.js
+++ script/singin.js
@@ -2,20 +2,32 @@ document.getElementById("signInBtn").addEventListener("click", signIn);
 
 function signIn() {
     let emailIn = document.getElementById("emailInput");
-    let passwordIn = document.getElementById("emailInput");
+    let passwordIn = document.getElementById("passwordInput");
+    let errorLabel = document.getElementById("errorMessage");
 
     let emailValid;
+    let displayError = false;
     let passwordValid = passwordIn.value != null && passwordIn.value != "" ? true : false;
 
     if (emailIn.value != null && emailIn.value != "")
         emailValid = validateEmail(emailIn.value);
 
     if (emailValid && passwordValid) {
-        // todo: valid email and password check for sign in
-        // todo: check if values are in local storage
-        // todo: if the value is in local storage add to cookies for one week
-    }
+        errorLabel.innerText = "";
+        let user = {
+            Email: emailIn.value,
+            Password: passwordIn.value
+        }
 
+        if (checkIfUserIsRegistered(user)) {
+            saveToCookies(user);
+            window.location.replace("/pages/dashboard.html");
+        }
+    }
+    else displayError = true;
+    if (displayError) {
+        errorLabel.innerText = "Wrong Email or Password!";
+    }
 }
 
 function validateEmail(email) {
@@ -69,4 +81,22 @@ function validateEmail(email) {
     }
 
     return true;
+}
+
+function checkIfUserIsRegistered(user) {
+    if (localStorage.getItem("registeredUsers")) {
+        let usersList = JSON.parse(localStorage.getItem("registeredUsers"));
+        for (const obj of usersList) {
+            if (obj.Email == user.Email && obj.Password == user.Password) return true;
+        }
+    }
+    return false;
+}
+
+function saveToCookies(user) {
+    let xHours = 2; // how many hours the user stays signed in
+    const d = new Date();
+    d.setTime(d.getTime() + (xHours * 60 * 60 * 1000));
+    let expires = d.toUTCString();
+    document.cookie = `signedInUser=${user.Email};expires=${expires};path=/`;
 }
