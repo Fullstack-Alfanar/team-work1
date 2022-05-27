11d0115604b16a7e3911422a6bb165023cd53c97
may-ha <101331038+may-ha@users.noreply.github.com>
2022-05-27 21:29:34 +0300

    loading index html page with check user cookie

---
+++ script/index.js
@@ -0,0 +1,24 @@
+function getCookies() {
+  let cookie = document.cookie;
+  let user = "";
+  if (cookie.includes("signedInUser")) {
+    let splitCookie = cookie.split(";");
+    for (let i = 0; i < splitCookie.length; i++) {
+      if (splitCookie[i].includes("signedInUser")) {
+        user = splitCookie[i].split("=")[1];
+      }
+    }
+  }
+  return user;
+}
+
+function checkUser() {
+  $(document).onload(() => {
+    let user = getCookies("signedInUser");
+    if (user != "") {
+      window.location.replace("./pages/dashborad.html");
+    } else {
+      window.location.replace("./pages/signin.html");
+    }
+  });
+}
