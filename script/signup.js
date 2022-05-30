1efb0b515f4b6db92948f5e35b781ae0026d0000
Yosef Awad <yosef.awad.1@gmail.com>
2022-05-30 08:15:28 +0300

    sign up functionality

--- script/signup.js
+++ script/signup.js
@@ -1,40 +1,55 @@
 function validateSignUp() {
   var userName = document.getElementById("username").value;
   var password = document.getElementById("password").value;
+  var confirmPassword = document.getElementById("passwordConfirm").value;
   var mail = document.getElementById("email").value;
-  let emailcharts = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  let passw = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
-  if (
-    userName == null ||
-    userName == "" ||
-    userName.length <= 2 ||
-    userName.length >= 9
-  ) {
-    alert("There's Filed Missing");
-    document.getElementById("username").style.borderColor = "red";
-  } else if (mail.match(emailcharts)) {
-    return true;
-  } else {
-    alert("You Have entered an invalid email address!");
-    return false;
+  let errorMsg = document.getElementById("Error");
+
+  let userNameValid = userName != null && userName != "" && userName.length > 2;
+  let mailValid = mail != null && mail != "" && validateEmail(mail);
+  let passwordValid = password != null && confirmPassword != null && validatePassword(password, confirmPassword);
+
+  if (userNameValid && mailValid && passwordValid) {
+    errorMsg.textContent = "";
+    const user = {
+      UserName: userName,
+      Password: password,
+      Email: mail,
+    };
+    addToLocalStorage(user);
+    window.location.href = "/pages/signin.html";
   }
-  if (password.match(passw) && password.length < 6) {
-    return true;
-  } else {
-    return false;
+  else {
+    errorMsg.textContent = "you have invalid fields!"
   }
-  addToLocalStorage();
 }
-function addToLocalStorage() {
+
+function addToLocalStorage(user) {
   const userNameEl = document.getElementById("username").value;
   const passwordEl = document.getElementById("password").value;
   const emailEl = document.getElementById("email").value;
+  let usersList = [];
 
-  const obj = {
-    username: userNameEl,
-    password: passwordEl,
-    email: emailEl,
-  };
-  arr.push(obj);
-  localStorage.setItem("List", JSON.stringify(arr));
+  if (localStorage.getItem("registeredUsers"))
+    usersList = JSON.parse(localStorage.getItem("registeredUsers"));
+  usersList.push(user);
+  localStorage.setItem("registeredUsers", JSON.stringify(usersList));
+}
+
+function validateEmail(email) {
+  var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+  if (email.match(validRegex)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function validatePassword(password, confirm) {
+  let passw = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
+  if (password.length < 6 && !password.match(passw) && passw != confirm) {
+    return false;
+  }
+  return true;
 }
